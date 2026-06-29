a4afa293c32bc47bb851422af980ea6e5b875949
Roman3233 <zmeiroma288@gmail.com>
2026-06-29 11:33:06 +0300

    feat: implement CommentsController

--- API/Controllers/CommentsController.cs
+++ API/Controllers/CommentsController.cs
@@ -15,7 +15,35 @@ public class CommentsController : ControllerBase
     [HttpGet]
     public async Task<IActionResult> GetAll()
     {
-        return Ok(await _context.Books.Include(b => b.User).Include(b => b.Comments).ToListAsync());
+        return Ok(await _context.Comments.Include(c=> c.User).Include(c => c.Book).ToListAsync());
     }
-    
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var comment = await _context.Comments
+            .Include(c => c.User)
+            .Include(c => c.Book)
+            .FirstOrDefaultAsync(c => c.Id == id);
+
+        return comment is null ? NotFound() : Ok(comment);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] Comment comment)
+    {
+        _context.Comments.Add(comment);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetById), new { id = comment.Id }, comment);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var comment = await _context.Comments.FindAsync(id);
+        if (comment is null) return NotFound();
+        _context.Comments.Remove(comment);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }    
 }
