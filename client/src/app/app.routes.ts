b261d414974131f1942c69f6862c034fe4bfb859
Roman3233 <zmeiroma288@gmail.com>
2026-07-18 18:22:18 +0300

    feat: implement user registration page and add registration route to application router

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { BookList } from './pages/book-list/book-list';
 import { BookDetail } from './pages/book-detail/book-detail';
 import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
 
 export const routes: Routes = [
     { path: '', component: BookList },
     { path: 'book/:id', component: BookDetail },
-    { path: 'login', component: Login }
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
 ];
--- client/src/app/pages/register/register.ts
+++ client/src/app/pages/register/register.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Auth } from '../../services/auth';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+  private authService = inject(Auth);
+  private router = inject(Router);
+  isLoading = signal(false);
+  username = '';
+  email = '';
+  password = '';
+
+  onRegister() {
+    this.isLoading.set(true);
+    this.authService.register(this.email, this.username, this.password).subscribe({
+      next: (data) => {
+        this.router.navigate(['/login']);
+        this.isLoading.set(false);
+      },
+      error: (err) => {
+        console.log(err);
+        this.isLoading.set(false);
+      },
+      complete: () => {
+        this.isLoading.set(false);
+      }
+    })
+  }
+}
