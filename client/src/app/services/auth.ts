f6bc6f25a385e5209aac318216089aadd191a7a5
Roman3233 <zmeiroma288@gmail.com>
2026-07-18 17:40:26 +0300

    feat: implement login page component and authentication service for user access

--- client/src/app/services/auth.ts
+++ client/src/app/services/auth.ts
@@ -1,6 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Auth {}
+export class Auth {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:5164/api/auth';
+
+  register(email: string, username: string, password: string) {
+    return this.http.post(this.apiUrl + '/register', { email, username, password });
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<{ token: string }>(this.apiUrl + '/login', { email, password });
+  }
+}
