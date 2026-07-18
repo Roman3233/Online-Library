dffb62935aeb0c55bcf9a5868cfe828f6690a0d6
Roman3233 <zmeiroma288@gmail.com>
2026-07-18 14:55:28 +0300

    feat: add BookService and initialize BookList component with HTTP client configuration

--- client/src/app/services/book.ts
+++ client/src/app/services/book.ts
@@ -1,6 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Book {}
+export class BookService {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:5164/api';
+
+  getAllBooks() {
+    return this.http.get<Book[]>(this.apiUrl + '/books');
+  }
+}
+
+export interface Book {
+  id: number;
+  title: string;
+  uploadedAt: Date;
+  userId: number;
+  contentType: string;
+  fileName: string;
+  filePath: string;
+  fileSize: number;
+}
