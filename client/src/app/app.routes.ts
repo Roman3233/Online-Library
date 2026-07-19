8af41c5dc445d4833f193d42a7a216c77ea0a354
Roman3233 <zmeiroma288@gmail.com>
2026-07-19 12:25:04 +0300

    feat: implement add-book page and register its route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { BookList } from './pages/book-list/book-list';
 import { BookDetail } from './pages/book-detail/book-detail';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
+import { AddBook } from './pages/add-book/add-book';
 
 export const routes: Routes = [
     { path: '', component: BookList },
     { path: 'book/:id', component: BookDetail },
     { path: 'login', component: Login },
     { path: 'register', component: Register },
+    { path: 'add-book', component: AddBook }
 ];
