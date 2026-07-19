0d40b8aa6360d3168b36c0ddbe2851d3e7b7cdc7
Roman3233 <zmeiroma288@gmail.com>
2026-07-19 12:13:24 +0300

    feat: add component for creating books and uploading associated files

--- client/src/app/pages/add-book/add-book.ts
+++ client/src/app/pages/add-book/add-book.ts
@@ -11,8 +11,38 @@ import { Router } from '@angular/router';
 })
 export class AddBook {
   private bookService = inject(BookService);
+  private router = inject(Router);
   title = '';
   selectedFile: File | null = null;
 
+  onFileChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      this.selectedFile = input.files[0];
+    }
+  }
 
+  onSubmit() {
+    this.bookService.createBook(this.title).subscribe({
+      next: (data) => {
+        this.bookService.uploadFile(data.id, this.selectedFile!).subscribe({
+          next: () => {
+            this.router.navigate(['/book/' + data.id]);
+          },
+          error: (err) => {
+            console.log(err);
+          },
+          complete: () => {
+            console.log('File uploaded');
+          }
+        });
+      },
+      error: (err) => {
+        console.log(err);
+      },
+      complete: () => {
+        console.log('Book created');
+      }
+    })
+  }
 }
