b418af9ccb455449b0922f62074508c401b6977e
Roman3233 <zmeiroma288@gmail.com>
2026-07-19 14:52:26 +0300

    feat: implement user service and profile component structure

--- client/src/app/services/user.ts
+++ client/src/app/services/user.ts
@@ -7,4 +7,20 @@ import { HttpClient } from '@angular/common/http';
 export class UserService {
   private http = inject(HttpClient);
   private apiUrl = 'http://localhost:5164/api/user';
+
+  getMyProfile() {
+    return this.http.get<User>(this.apiUrl + '/me');
+  }
+  getProfile(id: number) {
+    return this.http.get<User>(this.apiUrl + '/' + id);
+  }
+}
+
+export interface User {
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+  role: string;
+  registeredAt: Date;
 }
